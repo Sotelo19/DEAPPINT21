0ef50754dec12b7d4334a21256c5932b19b8eba2
Sotelo19 <sotelogarcia.egs@gmail.com>
2021-08-26 04:06:24 -0500

    va

---
+++ p03-areatriangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace p03_areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float labase, laaltura, elarea;
+            labase=laaltura=elarea=0;
+            Console.WriteLine("calculando el area de un triangulo\n");
+            Console.WriteLine("dame la base: ");
+            labase=float.Parse(Console.ReadLine());
+            Console.Write("dame la altura: ");
+            laaltura=float.Parse(Console.ReadLine());
+            elarea=(labase*laaltura)/2;
+            Console.WriteLine($"el area del triangulo es {elarea}");
+        }
+    }
+}
