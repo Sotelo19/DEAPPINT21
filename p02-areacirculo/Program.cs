ae22ee5c2b6c231068b9b985274a3433098cb9d9
Sotelo19 <sotelogarcia.egs@gmail.com>
2021-08-26 03:50:11 -0500

    va

---
+++ p02-areacirculo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace p02_areacirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area=0;
+            float radio=0;
+            Console.WriteLine("Calcular e area de un circulo");
+            Console.WriteLine("dame el radio?");
+            radio= float.Parse (Console.ReadLine());
+            area=Math.PI*Math.Pow(radio,2);
+            Console.WriteLine($"el area del circulo es {area}");
+
+        }
+    }
+}
