09602be32e8de32466e4755c2c4f1e869ef8a45d
Sotelo19 <sotelogarcia.egs@gmail.com>
2021-08-26 04:19:37 -0500

    va

---
+++ p04-pagatrabajador/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace p04_pagatrabajador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string nombre, salida;
+           int horas;
+           float pago,pagobruto,impuesto,pagoneto;
+           const float TASA=0.3f;
+           Console.WriteLine("calculando el pago del trabajador\n");
+           Console.WriteLine("dame tu nombre: "); nombre= Console.ReadLine();
+           Console.WriteLine("horas trabajadas: "); horas= int.Parse(Console.ReadLine());
+           Console.WriteLine("pago en horas: "); pago= float.Parse(Console.ReadLine());
+           pagobruto= horas*pago;
+           impuesto=pagobruto*TASA;
+           pagoneto=pagobruto-impuesto;
+
+           salida = $"el trabajador {nombre}. trabajo {horas}, con un pago de {pago} \n"+
+           $"pago bruto: {pagobruto} \n impuesto: {impuesto}\n pago neto: {pagoneto}";
+
+           Console.WriteLine(salida);
+        }
+    }
+}
