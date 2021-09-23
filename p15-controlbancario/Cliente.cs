415ad42743389e01724cb3fdbd29bcf781ad5095
Sotelo19 <sotelogarcia.egs@gmail.com>
2021-09-23 02:06:05 -0500

    v

---
+++ p15-controlbancario/Cliente.cs
@@ -0,0 +1,12 @@
+namespace p15_controlbancario{
+    public class Cliente{
+        public Cliente(string nombre, string apeido){
+            this.nombre=nombre;
+            this.apeido=apeido;
+        }
+
+        public string nombre {get; private set;}
+        public string apeido {get; private set;}
+        public CuentaBancaria cuenta {get;set;}
+    }
+}
